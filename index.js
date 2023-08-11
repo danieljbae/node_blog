b7439593da4dcca84630e89c136f5092918b9e70
Daniel Bae <danieljbaee@hotmail.com>
2023-08-10 22:43:58 -0700

    init commit: basic project file structure, downloaded bootstrap

---
+++ index.js
@@ -0,0 +1,27 @@
+// Imports
+const express = require('express')
+const path = require('path')
+
+// Init App server
+const app = express()
+
+// Register folder for static assets
+app.use(express.static('public'))
+
+// Create route handlers
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "pages/index.html"))
+})
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "pages/about.html"))
+})
+
+app.get("/contact", (req, res) => {
+    res.sendFile(path.join(__dirname, "pages/contact.html"))
+})
+
+//  Start App Server
+app.listen(port = 4000, () => {
+    console.log('App listening on Port 4000')
+})
