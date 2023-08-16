8c37f8b0094bce77fa0ccf4b7bf64e06d11084d3
Daniel Bae <danieljbaee@hotmail.com>
2023-08-15 17:15:23 -0700

    Added: form validation middleware

--- db/models/Post.js
+++ db/models/Post.js
@@ -7,10 +7,12 @@ const PostSchema = new mongoose.Schema({
     subtitle: String,
     content: String,
     username: String,
+    image: String,
     createdAt: {
         type: Date,
         default: new Date()
-    }
+    },
+
 })
 // Define a model (table) which app interacts with
 const Post = mongoose.model("Post", PostSchema);
--- index.js
+++ index.js
@@ -6,7 +6,9 @@ import express from 'express';
 import expressEdge from 'express-edge';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import fileUpload from 'express-fileupload';
 import Post from './db/models/Post.js';
+
 
 // Get the current filename and directory using ESM-compatible functions
 const __filename = fileURLToPath(import.meta.url);
@@ -15,12 +17,23 @@ const __dirname = dirname(__filename);
 // Init App server
 const app = express();
 
-// Add Functionality to app
-app.use(express.static('public')); // Register static assets
+// Register Middleware Packages 
+app.use(fileUpload()); // Adds files to our repsonse object
+app.use(express.static('public')); // Location of static assets
 app.use(expressEdge); // Templating engine functionality
 app.set('views', path.join(__dirname, 'views')); // Set location of views
 app.use(bodyParser.json()); // Allows our app to accept JSON from brower (or api client)
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Custom middleware - Validate form submission
+const validateCreatePostMiddleware = (req, res, next) => {
+    if (!req.body.username || !req.body.title || !req.body.subtitle || !req.body.content || !req.files || !req.files.image) {
+        return res.redirect('/post/new'); // Redirect to the new post creation page
+    }
+    next();
+};
+app.use("/post/store", validateCreatePostMiddleware);
+
 
 // Set up Database connection to MongoDB
 mongoose.connect('mongodb://localhost/node-js-blog');
@@ -38,37 +51,51 @@ app.get("/post/new", (req, res) => {
     res.render('create');
 });
 
-// Post request - save data to Posts model
 app.post("/post/store", async (req, res) => {
     try {
-        const post = await Post.create(req.body);
-        res.redirect('/');
+        const { image } = req.files;
+        const uploadPath = path.resolve(__dirname, 'public', 'posts', image.name);
+
+        image.mv(uploadPath, (error) => {
+            if (error) {
+                console.error("Error uploading image:", error);
+                return res.status(500).send("An error occurred while uploading the image");
+            }
+
+            const postData = {
+                ...req.body,
+                image: `/posts/${image.name}`,
+            };
+
+            Post.create(postData)
+                .then(() => {
+                    res.redirect('/');
+                }).catch((postCreateError) => {
+                    console.error("Error creating post:", postCreateError);
+                    res.status(500).send("An error occurred while creating the post");
+                });
+        });
     } catch (error) {
-        console.error(error);
+        console.error("Error:", error);
         res.redirect('/error-page'); // Redirect to an error page if something goes wrong
     }
 });
 
 app.get("/post/:id", async (req, res) => {
     try {
-        console.log("TESTING HELLO WORLD");
         const post = await Post.findById(req.params.id);
         if (!post) {
             // Handle the case where no post was found with the given ID
-            console.log("Unable to find post by ID!!!!!!!!");
             return res.status(404).send("Post not found");
         }
         res.render('post', {
             post
         });
-        console.log("Loaded in standard post ");
     } catch (error) {
         // Handle the error appropriately, e.g., sending an error response
         res.status(500).send("An error occurred while fetching the post");
-        console.log("CATCH ERROR", error);
     }
 });
-
 
 app.get("/about", (req, res) => {
     res.render('about');
